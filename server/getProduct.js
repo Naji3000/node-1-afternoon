6a5f81eaf79ad10e7cf4c6eefa3ed763514255fe
Naji Ali <SupremeBinxx@MacBook-Pro.local>
2019-08-26 13:53:04 -0500

    Fixed

--- server/getProduct.js
+++ server/getProduct.js
@@ -4,12 +4,12 @@ const products = require("../products.json")
 const getProduct = (req, res, next) => {
 
     const prod = products.find( prod => prod.id === parseInt(req.params.id));
-    if(prod) {
+    if(!prod) {
         return res.status(500).json("Item not in list")
-    } else {
+    } 
         res.status(200).json(prod)
+
     }
-}
 
 
 module.exports = getProduct;
